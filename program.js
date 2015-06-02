b4b27d168fb1149a84ce24777e355c1684863d10
Ryan B Leach <rleach@ebsco.com>
2015-06-02 17:15:13 -0400

    working deleter

---
+++ program.js
@@ -0,0 +1,93 @@
+#! /usr/bin/env node
+
+var exec = require('child_process').exec,
+	prompt = require('prompt'),
+	semver = require('semver'),
+	userArgs = require('minimist')(process.argv.splice(2)),
+	remote = userArgs.origin || 'origin',
+	pattern = userArgs.pattern,
+	versionRegExp = new RegExp(userArgs.versionRegExp || '(v[^\^{}^\n]+)'),
+	tagsToDelete = [],
+	tagsToIgnore = [],
+
+	commands = {
+		list: 'git ls-remote -t ' + remote,
+		deleteRemote: 'git push ' + remote + ' :refs/tags/',
+		deleteLocal: 'git tag -d '
+	},
+
+	prompts = {
+		confirm: {
+			name: 'okay',
+			default: 'no',
+			pattern: /^yes|no$/,
+			required: true,
+			description: 'Delete these tags?'.red
+		},
+		reallyConfirm: {
+			name: 'okay',
+			default: 'no',
+			pattern: /^yes|no$/,
+			required: true,
+			description: ('Really delete all these tags?\n' + tagsToDelete.join(', ')).red
+		}
+	},
+
+	tag_iterator = function(tag) {
+		tag = tag.match(versionRegExp);
+		if (tag) {
+			if (pattern && semver.satisfies(tag[1], pattern) && tagsToDelete.indexOf(tag[1]) === -1) {
+				tagsToDelete.push(tag[1]);
+			} else if (tagsToIgnore.indexOf(tag[1]) === -1){
+				tagsToIgnore.push(tag[1]);
+			}
+		}
+	},
+
+	cb_delete = function(err, answer1) {
+		if (answer1.okay === 'yes') {
+			prompt.get([prompts.reallyConfirm], cb_reallyDelete);
+		}
+	},
+
+	cb_reallyDelete = function(err, answer2) {
+		var locals = [],
+			remotes = [];
+		if (answer2.okay === 'yes') {
+			tagsToDelete.forEach(function(tag) {
+				locals.push(commands.deleteLocal + tag);
+				remotes.push(commands.deleteRemote + tag);
+			});
+		}
+		exec(locals.join(' && '), cb_locals);
+		exec(remotes.join(' && '), cb_remotes);
+	},
+
+	cb_locals = function(err, stdout, stderr) {
+		if (err || stderr) {
+			console.error('failed to delete locals', err, stderr);
+		}
+	},
+
+	cb_remotes = function(err, stdout, stderr) {
+		if (err) {
+			console.error('failed to delete remotes', err);
+		}
+	};
+
+prompt.start();
+
+exec(commands.list, function(err, stdout, stderr) {
+
+	stdout.split(/\n/).forEach(tag_iterator);
+
+	if (tagsToDelete.length) {
+		console.log('Matching pattern:', pattern);
+		console.log(tagsToDelete.join(', '));
+		prompt.get([prompts.confirm], cb_delete);
+	} else {
+		console.log(tagsToIgnore.join(', '));
+		console.log('No matches. Listing all the tags.'.blue);
+	}
+
+});
